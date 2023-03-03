17f7d70de4515881ec327342489b7e43bd38e811
Nwigwe Chidubem <krain9421@gmail.com>
2023-03-03 09:36:06 +0000

    Modified Task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -10,7 +10,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	func(tree->n);
 	binary_tree_inorder(tree->left, func); /*Recursively traverse left*/
+	func(tree->n);
 	binary_tree_inorder(tree->right, func); /*Recursively traverse right*/
 }
