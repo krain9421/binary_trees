56b41a4cf1cf0231c6ad8687ba8605777f09aaeb
Nwigwe Chidubem <krain9421@gmail.com>
2023-03-03 09:32:56 +0000

    Tasks 7 & 8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -12,5 +12,5 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 
 	binary_tree_postorder(tree->left, func); /*Recursively traverse left*/
 	binary_tree_postorder(tree->right, func); /*Recursively traverse right*/
-	funct(tree->n);
+	func(tree->n);
 }
