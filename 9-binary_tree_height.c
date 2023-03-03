7cd9a7c08e8083a6b423adeae1b0d9458e22b5e8
Nwigwe Chidubem <krain9421@gmail.com>
2023-03-03 10:11:07 +0000

    Modified 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#define max(a,b) ((a) > (b) ? (a) : (b))
 
 /**
  * binary_tree_height - returns the height of a binary tree
@@ -22,5 +23,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	right_height = binary_tree_height(tree->right);
 
 	/*Return the maximum of left & right height*/
-	return (max(left_height, right_height) + 1);
+	return max(left_height, right_height) + 1;
 }
