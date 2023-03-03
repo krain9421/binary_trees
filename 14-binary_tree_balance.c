8aa57912d0cb1e151a9c0a187c2ce0a4fbfaa975
Nwigwe Chidubem <krain9421@gmail.com>
2023-03-03 14:37:20 +0000

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -17,18 +17,19 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	/*Get the height of left and right sub-trees*/
-	if (tree->left)
-		left_height += 1 + binary_tree_height(tree->left);
-	if (tree->right)
-		right_height += 1 + binary_tree_height(tree->right);
+	/*if (tree->left)*/
+	left_height += 1 + binary_tree_height(tree->left);
+	/*if (tree->right)*/
+	right_height += 1 + binary_tree_height(tree->right);
 
 	/*Return the maximum of left & right height*/
 	return (max(left_height, right_height));
 }
 
-/*
+/**
  * binary_tree_balance - A function that measures the balance factor
  * of a binary tree.
+ *
  * @tree: Pointer to root node to calculate balance factor.
  *
  * Return: The balance factor of the tree.
