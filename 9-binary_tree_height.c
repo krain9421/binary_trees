31b2fae032b3d007b9e1d9b277bac6f94fa53d22
Nwigwe Chidubem <krain9421@gmail.com>
2023-03-03 10:18:49 +0000

    Modified 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -18,10 +18,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	/*Get the height of left and right sub-trees*/
 	if (tree->left)
-		left_height += binary_tree_height(tree->left);
+		left_height += 1 + binary_tree_height(tree->left);
 	if (tree->right)
-		right_height += binary_tree_height(tree->right);
+		right_height += 1 + binary_tree_height(tree->right);
 
 	/*Return the maximum of left & right height*/
-	return (max(left_height, right_height) + 1);
+	return (max(left_height, right_height));
 }
